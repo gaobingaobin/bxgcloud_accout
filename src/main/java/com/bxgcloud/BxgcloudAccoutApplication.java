7e07ac082b8440f101fec518c13d9ebb99af232c
gaobin <gaobin@workway.com.cn>
2016-11-07 17:19:30 +0800

    frist

---
+++ src/main/java/com/bxgcloud/BxgcloudAccoutApplication.java
@@ -0,0 +1,12 @@
+package com.bxgcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxgcloudAccoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BxgcloudAccoutApplication.class, args);
+	}
+}
